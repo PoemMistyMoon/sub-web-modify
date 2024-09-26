358e5243e3b3b04f67f50faf94786890fd303056
PoemMistyMoon <167159446+PoemMistyMoon@users.noreply.github.com>
2024-09-26 17:38:33 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -193,15 +193,25 @@
                   </el-button>
                 </el-input>
               </el-form-item>
-             >生成订阅链接
+            >生成订阅链接
                 </el-button>
                 <el-button
-                    style="width: 250px"
+                    style="width: 120px"
                     type="danger"
                     @click="makeShortUrl"
                     :loading="loading1"
                     :disabled="customSubUrl.length === 0"
-                  >自定义配置
+                >生成短链接
+                </el-button>
+              </el-form-item>
+              <el-form-item label-width="0px" style="text-align: center">
+                <el-button
+                    style="width: 120px"
+                    type="primary"
+                    @click="dialogUploadConfigVisible = true"
+                    icon="el-icon-upload"
+                    :loading="loading2"
+                >自定义配置
                 </el-button>
                 <el-button
                     style="width: 120px"
